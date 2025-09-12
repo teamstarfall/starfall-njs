7ce8d25a266745b405b294d22759ea1681821f6b
azuretoast <kuyajc670@gmail.com>
2025-09-12 04:27:24 -0600

    added avatar icons

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -11,7 +11,7 @@ const AvatarComponent: React.FC<AvatarProps> = ({ _imgname, status }) => {
     <div className="flex justify-center relative mb-5">
       <img
         className="w-48 h-48 border-4 rounded-3xl z-0"
-        src={`/icons/${_imgname}`}
+        src={`icons/${_imgname}`}
         alt={`${_imgname}`}
       />
       <div className="absolute bottom-[-20px] left-1/2 -translate-x-1/2">

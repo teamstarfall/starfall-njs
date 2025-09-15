93fa2449c8fe98b02ee931f824ea8614b19ff666
AzureToast <kuyajc670@gmail.com>
2025-09-14 20:51:18 -0600

    added planets to background

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -62,7 +62,7 @@ const ParallaxBackground: React.FC<object> = () => {
         []
     );
 
-    const generateSunConfig = (name: string, color: string, size: number, force: number, smooth: number, zIndex: number) => {
+    const generateBodyConfig = (name: string, color: string, size: number, force: number, smooth: number, zIndex: number, posx: number, posy: number) => {
         const particleOptions = {
             background: {
                 color: "transparent",
@@ -79,9 +79,10 @@ const ParallaxBackground: React.FC<object> = () => {
                 shadow: {
                     enable: true, // Enable shadow effect
                     color: color, // Color of the shadow
-                    blur: 100, // Adjust the blur level
+                    blur: 50, // Adjust the blur level
                 },
             },
+    
             interactivity: {
                 events: {
                     onHover: {
@@ -96,7 +97,7 @@ const ParallaxBackground: React.FC<object> = () => {
             },
             manualParticles: [
                 {
-                    position: { x: 50, y: 50 },
+                    position: { x: posx, y: posy },
                 },
             ],
         };
@@ -123,15 +124,27 @@ const ParallaxBackground: React.FC<object> = () => {
                         options={starsConfig} // The particles configuration is memoized
                     />
                 </span>
-                {generateSunConfig("sun1", "rgba(255, 255, 255, 1)", 50, 950, 10, -501)}
-                {generateSunConfig("sun2", "rgba(81, 128, 113, 0.9)", 75, 1200, 10, -502)}
-                {generateSunConfig("sun3", "rgba(0, 144, 111, 0.8)", 120, 3000, 10, -503)}
-                {generateSunConfig("sun4", "rgba(200, 252, 234, 0.7)", 180, 5000, 10, -504)}
-                {generateSunConfig("sun5", "rgba(108, 186, 162, 0.6)", 250, 7000, 10, -505)}
-                {generateSunConfig("sun6", "rgba(0, 143, 145, 0.5)", 325, 9000, 10, -506)}
-                {generateSunConfig("sun7", "rgba(11, 144, 143, 0.4)", 400, 11000, 10, -507)}
-                {generateSunConfig("sun8", "rgba(56, 94, 82, 0.3)", 550, 13000, 10, -508)}
-                {generateSunConfig("sun9", "rgba(26, 37, 34, 0.2)", 750, 15000, 10, -509)}
+                <span>
+                    {generateBodyConfig("sun1", "rgba(255, 255, 255, 1)", 50, 15000, 10, -501, 50, 50)}
+                    {generateBodyConfig("sun2", "rgba(81, 128, 113, 0.9)", 75, 12500, 10, -502, 50, 50)}
+                    {generateBodyConfig("sun3", "rgba(0, 144, 111, 0.8)", 120, 10000, 10, -503, 50, 50)}
+                    {generateBodyConfig("sun4", "rgba(200, 252, 234, 0.7)", 180, 7500, 10, -504, 50, 50)}
+                    {generateBodyConfig("sun5", "rgba(108, 186, 162, 0.6)", 250, 6000, 10, -505, 50, 50)}
+                    {generateBodyConfig("sun6", "rgba(0, 143, 145, 0.5)", 325, 5500, 10, -506, 50, 50)}
+                    {generateBodyConfig("sun7", "rgba(11, 144, 143, 0.4)", 400, 5000, 10, -507, 50, 50)}
+                    {generateBodyConfig("sun8", "rgba(56, 94, 82, 0.3)", 550, 4500, 10, -508, 50, 50)}
+                    {generateBodyConfig("sun9", "rgba(26, 37, 34, 0.2)", 750, 4000, 10, -509, 50, 50)}
+
+
+                    {generateBodyConfig("planet1", "rgba(14, 46, 71, 1)", 500, 5000, 10, -401, 10, 100)}
+                    {generateBodyConfig("planet1bg", "rgba(45, 101, 144, 1)", 505, 5000, 10, -402, 10, 100)}
+                    {generateBodyConfig("planet2", "rgba(37, 80, 103, 1)", 200, 5000, 10, -403, 30, 65)}
+                    {generateBodyConfig("planet2bg", "rgba(36, 103, 140, 1)", 205, 5000, 10, -404, 30, 65)}
+                    {generateBodyConfig("planet2bg", "rgba(36, 103, 140, 1)", 205, 5000, 10, -404, 30, 65)}
+                    {generateBodyConfig("earth", "rgba(156, 218, 220, 1)", 10, 250, 10, -405, 73, 25)}
+                </span>
+                
+                
             </div>
         );
     }
